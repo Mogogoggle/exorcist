40e7ce8ea5a7f2b5ec827e1441813c9519a11db5
erenb <eren.bunul@students.frc5113.com>
2023-11-13 20:16:47 +0000

    buring ryna gosl

---
+++ chapter 3/exposed_nerve.py
@@ -0,0 +1,18 @@
+eminiem = """Dear slim, 
+I wrote you
+but you still ain't respondin'
+I left my cell phone 
+my address
+and my pager at the bottom
+
+
+
+the bottom
+ |
+ V
+ 609-870-1842
+ Behind you
+ idk what a pager is
+ Signed-
+ Slim"""
+print(eminiem)
